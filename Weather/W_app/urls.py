dcd700d7ad411309e01be85199b8d04dbe1f644d
Anukul Srivastava <anukulsrivastava123@gmail.com>
2023-08-24 09:34:15 +0530

    Update urls.py

--- Weather/W_app/urls.py
+++ Weather/W_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home),
-    path('static',views.static),
-    path('display',views.display)
+    # path('',views.home),
+    path('',views.static),
+    path('display',views.display),
+    path('discard/<str:roll>',views.discard),
+    path('edit/<str:roll>',views.edit),
+    path('update/<str:roll>',views.update)
 ]
