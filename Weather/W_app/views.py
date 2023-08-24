7910b49e62936b046b4ab0a3729775051ddc473d
Anukul Srivastava <anukulsrivastava123@gmail.com>
2023-08-24 09:34:11 +0530

    Update views.py

--- Weather/W_app/views.py
+++ Weather/W_app/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import requests
 from W_app.models import Student
+
+
 # Create your views here.
 
-def home(request):
-    if request.method == 'POST':
-        city = request.POST['city']
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=e1b0c38d394d992c06d331ce329389fa'
+# def home(request):
+#     if request.method == 'POST':
+#         city = request.POST['city']
+#         url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=e1b0c38d394d992c06d331ce329389fa'
 
-        response = requests.get(url)
-        weather_data = response.json()
-        city_update = {
-            'City': city,
-            'temperature' : int((weather_data['main']['temp'])-273.15),
-            'wind': weather_data['wind']['speed']
-        }
-        return render(request,'index.html',{'city_update':city_update})
-    else:
-        city_update=''
-        return render(request,'index.html',{'city_update':city_update})
+#         response = requests.get(url)
+#         weather_data = response.json()
+#         city_update = {
+#             'City': city,
+#             'temperature' : int((weather_data['main']['temp'])-273.15),
+#             'wind': weather_data['wind']['speed']
+#         }
+#         return render(request,'index.html',{'city_update':city_update})
+#     else:
+#         city_update=''
+#         return render(request,'index.html',{'city_update':city_update})
     
 def static(request):
     if request.method == 'POST':
@@ -35,3 +37,23 @@ def static(request):
 def display(request):
     ob = Student.objects.all()
     return render(request,'display.html',{'ob':ob})
+
+def discard(request,roll):
+    ob = Student.objects.get(roll = roll)
+    ob.delete()
+    return redirect('/display')
+
+def edit(request,roll):
+    task = Student.objects.get(roll=roll)
+    return render(request,'edit.html',{'task':task})
+
+def update(request,roll):
+    task = Student.objects.get(roll = roll)
+    task.name = request.POST['name']
+    task.roll = request.POST['roll']
+    task.email = request.POST['email']
+    task.mob = request.POST['phone']
+    task.save()
+    return redirect('/display')
+
+
